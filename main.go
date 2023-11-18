473338883aa4ac08f61a907248f5e61ab7de3830
Ubuntu <ubuntu@ip-172-31-11-117.us-east-2.compute.internal>
2023-11-18 07:43:47 +0000

    redid server for static file hosting and api endpoints

--- main.go
+++ main.go
@@ -1,16 +1,56 @@
 package main
 
 import (
-	"net/http"
 	"fmt"
+	"github.com/gin-gonic/gin"
+	"io/ioutil"
+	"path/filepath"
+	"net/http"
+	"strings"
 )
 
-func main() {
-	fs := http.FileServer(http.Dir("public"))
-	http.Handle("/", fs)
+type post struct {
+	Title    string `json:"title"`
+	Filename string `json:"filename"`
+}
 
-	err := http.ListenAndServe(":80", nil)
+func main() {
+	router := gin.Default()
+
+	router.Static("/static", "/home/ubuntu/public")
+
+	router.GET("/posts", getPosts)
+
+	router.Run(":80")
+}
+
+func getPosts(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, findPosts())
+}
+
+func findPosts() []post {
+	var posts []post
+
+	files, err := ioutil.ReadDir("/home/ubuntu/public")
 	if err != nil {
-		fmt.Println("Listent and server error: ", err)
+		fmt.Println(err)
 	}
+
+	for _, f := range files {
+		filename := f.Name()
+
+		if filepath.Ext(filename) == ".md" {
+			title := mapToTitle(filename)
+			posts = append(posts, post{Title: title, Filename: filename})
+		}
+	}
+
+	return posts
+}
+
+func mapToTitle(filename string) string {
+	title := strings.TrimSuffix(filename, ".md")
+
+	title = strings.ReplaceAll(title, "_", " ")
+	return title
 }
